3212d75c0f81935e614567e1af3e9e6651248e66
hienphamm <hienphamm@nashtechglobal.com>
2022-11-21 17:49:19 +0700

    Init React App

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "react-hooks"],
+  overrides: [],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+function App(): ReactElement {
+  return <h1>Vite + React</h1>;
+}
+
+export default App;
